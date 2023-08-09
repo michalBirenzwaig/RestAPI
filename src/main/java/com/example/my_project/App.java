8b755c287b722f861f496714efe06bc646876ad9
michalBirenzwaig <michalostrov488@gmail.com>
2023-08-09 22:54:44 +0300

    update files

--- src/main/java/com/example/my_project/App.java
+++ src/main/java/com/example/my_project/App.java
@@ -1,7 +1,5 @@
 package com.example.my_project;
 
-
-import com.example.my_project.WorkerApiClient.Worker;
 /**
  * Hello world!
  *
@@ -13,9 +11,6 @@ public class App
 	
     public static void main( String[] args ) throws Exception
     {
-    System.out.print("ffffff");
-     WorkerApiClient workerApiClient=new WorkerApiClient();
-     Worker worker=new Worker("1111111111","TESNET","20000",30);
-     workerApiClient.addNewWorker(worker);
+
     }
 }
